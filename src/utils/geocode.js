367d2dac0d902c8ca516987217fd2b1a5e20492c
vijayproject1 <vijayk.bura@gmail.com>
2020-01-08 23:06:38 +0530

    ADDED express

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,9 +1,11 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoidmlqYXkwNjAiLCJhIjoiY2s1NWtybjV1MGIyNTNtbzNsNXoweW9ybSJ9.riP4FAabVM9OtM1FiTrw-w'
 
     request({ url, json: true }, (error, { body }) => {
+        console.log(body)
+       console.log(body.features[0].center[1]);
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
